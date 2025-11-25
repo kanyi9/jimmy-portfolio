51631c00261ba95ba53c5c3f9a4278d2ea4d35a6
kanyi9 <peterkanyi19193@gmail.com>
2025-11-25 14:15:55 +0300

    adds whatsapp button

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Navbar from "./components/Navbar";
+import WhatsAppButton from "./components/WhatsappButton";
 import "./index.css";
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <WhatsAppButton/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
